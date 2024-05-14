f8a302686f42e9d264ef6ead50291b66b125a93e
shoba <shoba@SIYANDA>
2024-05-14 23:22:25 +0200

    Add project files.

---
+++ PersonalPortfolio1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalPortfolio1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PersonalPortfolio1/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using PersonalPortfolio1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalPortfolio1.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactForm model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                TempData["SuccessMessage"] = "Message sent successfully!";
+                return RedirectToAction("Contact");
+            }
+            return View(model);
+        }
+    }
+}
